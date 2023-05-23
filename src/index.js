cfd852a9fa6d0b2e89a833f103b53a26cb6e52c7
kasiaknapowska <k.knapowska@gmail.com>
2023-05-23 12:32:28 +0200

    footer styles, social icons

--- src/index.js
+++ src/index.js
@@ -1,14 +1,22 @@
-import './styles/main.scss'
-import { Form } from './js/form'
-import logo from './assets/logo.png'
+import './styles/main.scss';
+import { Form } from './js/form';
+import logo from './assets/logo.png';
+import facebook from './assets/icon-fb.svg';
+import instagram from './assets/icon-insta.svg';
 
 const logoImg = document.getElementById('logo');
 logoImg.src = logo;
+const fbIcon = document.getElementById('fb');
+fbIcon.src = facebook;
+const inIcon = document.getElementById('in');
+inIcon.src = instagram;
+
+
 
 
 const form = document.querySelector("form");
 
 if (form) {
     const fields = ["name", "phone", "email", "message", "acceptdata", "acceptpolicy"];
-    const validator = new Form(form, fields)
+    const validator = new Form(form, fields);
 }
