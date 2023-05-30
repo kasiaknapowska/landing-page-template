7a7e9cc50749b7163a857758cb84e1381d628887
kasiaknapowska <k.knapowska@gmail.com>
2023-05-30 13:10:37 +0200

    some styles

--- src/index.js
+++ src/index.js
@@ -1,31 +1,45 @@
-import './styles/main.scss';
-import { Form } from './js/form';
-import { ScrollToTop } from './js/scrollToTop';
-import logo from './assets/logo.png';
-import facebook from './assets/icon-fb.svg';
-import instagram from './assets/icon-insta.svg';
-import youtube from './assets/icon-yt.svg';
+import "./styles/main.scss";
+import { Form } from "./js/form";
+import { ScrollToTop } from "./js/scrollToTop";
+import logo from "./assets/logo.png";
+import facebook from "./assets/icon-fb.svg";
+import instagram from "./assets/icon-insta.svg";
+import youtube from "./assets/icon-yt.svg";
 
-const logoImg = document.getElementById('logo');
+const logoImg = document.getElementById("logo");
 logoImg.src = logo;
-const fbIcon = document.getElementById('fb');
+const fbIcon = document.getElementById("fb");
 fbIcon.src = facebook;
-const inIcon = document.getElementById('in');
+const inIcon = document.getElementById("in");
 inIcon.src = instagram;
-const ytIcon = document.getElementById('yt');
+const ytIcon = document.getElementById("yt");
 ytIcon.src = youtube;
-
-
 
 const form = document.querySelector("#contact-form");
 
 if (form) {
-    const fields = ["name", "phone", "email", "message", "acceptdata", "acceptpolicy"];
-    const validator = new Form(form, fields);
+  const fields = [
+    "name",
+    "phone",
+    "email",
+    "message",
+    "acceptdata",
+    "acceptpolicy",
+  ];
+  const validator = new Form(form, fields);
+
+  const tooltips = document.getElementsByClassName("tooltip");
+  console.log(tooltips);
+  tooltips.forEach((tooltip) => {
+    tooltip.parentElement.addEventListener("click", () => {
+        tooltip.style.visibility = "visible" ? "hidden" : "visible";
+        tooltip.style.opacity = "1" ? "0" : "1";
+    });
+  });
 }
 
 const scrollToTopBtn = document.querySelector("#scrollToTop");
 
 if (scrollToTopBtn) {
-    const top = new ScrollToTop(scrollToTopBtn);
+  const top = new ScrollToTop(scrollToTopBtn);
 }
